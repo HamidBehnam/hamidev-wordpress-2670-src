6db8eb034696af1e4cdd93aa0a32caf2859f3169
Hamid Behnam <hbehnam@legalzoom.com>
2020-06-27 17:54:01 -0700

    adding the Advanced Custom Field plugin, adding a custom field to the organiation post type

--- public/wp-content/themes/hamidev-wordpress-100/single-organization.php
+++ public/wp-content/themes/hamidev-wordpress-100/single-organization.php
@@ -1,8 +1,11 @@
 <?php
 get_header();
+the_post();
 ?>
 
 <div>this is going to show the single organization.</div>
+
+<div>the size of this organization is: <?php the_field('organization_size'); ?></div>
 
 <?php
 get_footer();
